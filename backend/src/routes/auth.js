cb51aa6b9320b632589954b5cba7cbae5b31f932
wbach <wassim.bacha@adventori.com>
2026-01-23 14:57:09 +0100

    dumb: also this

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const usersController = require('../controllers/usersController');
 const authMiddleware = require('../middleware/authMiddleware');
 const {
   validateRegister,
